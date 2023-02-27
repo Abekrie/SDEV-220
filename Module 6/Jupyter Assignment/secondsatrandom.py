a0246e616d99bdbc43580877b533f37d792599aa
paul <paul@helpmepaul.com>
2023-02-27 18:17:28 -0500

    Jupyter Assignment added

---
+++ Module 6/Jupyter Assignment/secondsatrandom.py
@@ -0,0 +1,15 @@
+import multiprocessing
+
+def now(seconds):
+    from datetime import datetime
+    from time import sleep
+    sleep(seconds)
+    print('wait', seconds, 'seconds, time is', datetime.utcnow())
+    
+if __name__ == '__main__':
+    import random
+    for n in range(3):
+        seconds = random.random()
+        now(seconds)
+        proc = multiprocessing.Process(target=now, args=(seconds,))
+        proc.start()
